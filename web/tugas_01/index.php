744c2878733488fe52629ae47b55ca18609a94ad
WaodeMakaniDaga <basriyasin@gmail.com>
2017-11-04 18:06:23 +0800

    tugas_01 WEB  buat Form Login

---
+++ web/tugas_01/index.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Belajar Bicara Dengan Komputer</title>
+    </head>
+    <body>
+        <h3>Belajar Bicara Dengan Komputer</h3>
+
+        <table>
+            <form>
+                <tr>
+                    <td>
+                        Nama Lengkap :
+                    </td>
+                    <td>
+                        <input type="text" name="namalengkap">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Email :
+                    </td>
+                    <td>
+                        <input type="text" name="namalengkap">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Daftar" name="daftar">
+                    </td>
+                </tr>
+            </form>
+        </table>
+    </body>
+</html>
+
