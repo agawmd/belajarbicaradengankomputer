c1f5543cc65b8738e02e397bf0c7aa8aede4bfc7
Basri Yasin <basriyasin@gmail.com>
2017-11-07 21:24:24 +0700

    update pbo_prak_iv Tue, Nov  7, 2017  9:24:24 PM

--- java/52012049_Mahasiswa_PBO_Praktikum_VI/src/mahasiswa/Mahasiswa.java
+++ java/52012049_Mahasiswa_PBO_Praktikum_VI/src/mahasiswa/Mahasiswa.java
@@ -14,13 +14,48 @@ import strukturdata.DataMahasiswa;
  */
 public class Mahasiswa {
 
+    
+    /**
+     * Menimpan data mahasiswa
+     * kemudian mengembalikan indikator keberhasilan
+     * saat menyimpan data
+     * 
+     * @param   nim                 nim dari mahasiswa baru
+     * @param   nama                nama dari mahasiswa baru
+     * @param   prodi               prodi dari mahasiswa baru
+     * @return  boolean
+     */
     public boolean simpanData(String nim, String nama, String prodi) {
         boolean berhasil = false;
         
+        // simpan data mahasiswa 
+        // jika berhasil menyimpan data set variabel 'berhasil' menjadi 'true'
+        // jika tidak berhasil menyimpan data set variabel 'berhasil' menjadi 'false'
+        
         return berhasil;
     }
+
     
-    public List<DataMahasiswa> getData(String nim) {
+    /**
+     * mengambil data mahasiswa berdasarkan nim
+     * 
+     * @param   nim                 nim mahasiswa
+     * @return  DataMahasiswa       detail mahasiswa
+     */
+    public DataMahasiswa getData(String nim) {
+        DataMahasiswa data = null;
+        
+        
+        return data;
+    }
+    
+    
+    /**
+     * mengambil semua data mahasiswa
+     * 
+     * @return  List<DataMahasiswa>     array dari data mahasiswa
+     */
+    public List<DataMahasiswa> getAllData() {
         List<DataMahasiswa> data = null;
         
         
