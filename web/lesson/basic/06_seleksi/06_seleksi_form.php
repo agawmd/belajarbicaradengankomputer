2c1eb14249520be9fb996c6ea6cfa14eaaf07970
Basri Yasin <basriyasin@gmail.com>
2017-11-19 23:34:04 +0800

    Checked

--- web/lesson/basic/06_seleksi/06_seleksi_form.php
+++ web/lesson/basic/06_seleksi/06_seleksi_form.php
@@ -34,7 +34,21 @@
          <hr>
         </pre>
         <!--   Tulis dibawah   -->
-
+        Input<br>
+        <form action="06_seleksi_action.php" method="post">
+            <label>Kacang   :</label>
+            <input type="text" name="kacang" placeholder="Jumlah"/> Kg<br><br>
+            <label>Pisang   :</label>
+            <input type="text" name="pisang" placeholder="Jumlah"/> Tandan<br><br>
+            <label>Terong   :</label>
+            <input type="text" name="terong" placeholder="Jumlah"/> Buah<br><br>
+            <label>Mentega   :</label>
+            <input type="text" name="mentega" placeholder="Jumlah"/> Kg<br><br>
+            <label>Tepung   :</label>
+            <input type="text" name="tepung" placeholder="Jumlah"/> Kg
+            
+            <input type="submit" name="belanja" value="Beli Ini"/>
+        </form>
 
 
     </body>
