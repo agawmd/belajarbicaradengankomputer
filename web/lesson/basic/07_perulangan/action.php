bfa5c736e8c612561618458da0c51c4128c3c1c8
Basri Yasin <basriyasin@gmail.com>
2017-11-20 17:18:10 +0700

    add web/basic/sectionlib

---
+++ web/lesson/basic/07_perulangan/action.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Put Your code here...
+ */
+
+
+
+
+
+
+?>
+
+<html>
+    <body>
+
+        <!-- Put your code here... -->
+
+        <hr>
+        <a href="idex.php"/>Back
+    </body>
+</html>
+
---
+++ web/lesson/basic/07_perulangan/index.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <!-- Quest section -->
+        <?php
+            require_once '../sectionlib/section.php';
+            section::printQuest(__DIR__);
+        ?>
+        <!-- End of section -->
+
+
+
+        <form action="action.php" method="post">
+            <!--   Put your cede here   -->
+        </form>
+
+        
+    </body>
+</html>
