78735c858ca403810ed0287fe1b07abdea8950a3
basriyasin <basriyasin@gmail.com>
2017-11-24 08:19:06 +0800

    Checked

--- java/MID_SISTER/MidSister/src/mid_52012049/Manager.java
+++ java/MID_SISTER/MidSister/src/mid_52012049/Manager.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mid_52012049;
 
 import java.io.IOException;
 import java.io.*;
 import java.net.*;
 
-/**
- *
- * @author BzKurozaki
- */
 public class Manager {
 
     DataInputStream managerTerima;
--- java/MID_SISTER/MidSister/src/mid_52012049/Persegi.java
+++ java/MID_SISTER/MidSister/src/mid_52012049/Persegi.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mid_52012049;
 import java.io.IOException;
 import java.io.*;
 import java.net.*;
 import javax.swing.SwingUtilities;
 
-
-/**
- *
- * @author BzKurozaki
- */
 public class Persegi {
 
     public DataInputStream  persegiTerima;
