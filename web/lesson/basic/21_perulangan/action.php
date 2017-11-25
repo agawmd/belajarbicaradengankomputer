a554f71e2f532861699c1aaf472f8739ea785ba3
basriyasin <basriyasin@gmail.com>
2017-11-25 20:22:41 +0800

    Checked

---
+++ web/lesson/basic/21_perulangan/action.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Put Your code here...
+ */
+
+$ui = $_POST['bb'];
+
+
+?>
+
+<html>
+    <body>
+
+        <!-- Put your code here... -->
+        <?php
+        for ($i=1; $i<=$ui; $i++) {
+            if ($i % 2 == 1) {
+                $i;
+            }
+        }
+        ?>
+        
+        
+    </body>
+</html>
+
