82ff1a78473e5561ef39d0263003e59df934aedf
basriyasin <basriyasin@gmail.com>
2018-03-03 11:06:02 +0800

    update Sab Mar  3 11:06:02 WITA 2018

--- web/Naruto/resources/views/personal/beranda/home.blade.php
+++ web/Naruto/resources/views/personal/beranda/home.blade.php
@@ -3,12 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <img src="/img/no.JPG">
-        <div class="hero-image">
-            <div class="hero-text">
-                <h1>I am Aga</h1>
-                <p>This is me and my Life so fuck up</p>
-            </div>
+        <div class="hero-text">
+            <h1>I am Aga</h1>
         </div>
     </div>
 </div>   
