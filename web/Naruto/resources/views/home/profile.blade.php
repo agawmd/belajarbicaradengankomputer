b11f69a29bc7b8c17150e862f693d563465b0b1a
basriyasin <basriyasin@gmail.com>
2018-03-08 15:00:42 +0800

    Checked

--- web/Naruto/resources/views/home/profile.blade.php
+++ web/Naruto/resources/views/home/profile.blade.php
@@ -6,6 +6,62 @@
 
 @section('content')
 <div class="row">
+    
+    <div class="col-md-6" style="font-family: monospace;">
+        <img class="photo" src="{{ asset('/img/nana.jpg') }}" alt="{{ asset('/img/nana.jpg') }}">
+        <div class="box">
+            <h1>BIODATA DIRI</h1>
+        </div>
+
+        <div class="text-box">
+            <div class="text-tx">Nama Lengkap : Masdiana Arifin</div>
+            <div class="text-tx">Tempat/Tgl Lahir : Polewali Mandar, ... </div>
+            <div class="text-tx">Alamat : BTN Minasa Sari Blok ..</div>
+            <div class="text-tx">E-mail : geumnhana@gmail.com</div>
+            <div class="text-tx">Phone Number : +62852 **** ****</div>
+        </div>        
+    </div>
+    
+    <div class="col-md-6" style="font-family: monospace;">
+        <img class="photo" src="{{ asset('/img/basri.jpg') }}" alt="{{ asset('/img/basri.jpg') }}">
+        <div class="box">
+            <h1>BIODATA DIRI</h1>
+        </div>
+
+        <div class="text-box">
+            <div class="text-tx">Nama Lengkap : Basri Yasin</div>
+            <div class="text-tx">Tempat/Tgl Lahir : Gorontalo, 08 Januari 1994 </div>
+            <div class="text-tx">Alamat : Jl. Urip Sumoharjo ..</div>
+            <div class="text-tx">E-mail : basriyasin@gmail.com</div>
+            <div class="text-tx">Phone Number : +62857 **** ****</div>
+        </div>        
+    </div>
+    
+    <div class="col-md-6" style="font-family: monospace;">
+        <img class="photo" src="{{ asset('/img/aga.jpg') }}" alt="{{ asset('/img/aga.jpg') }}">
+        <div class="box">
+            <h1>BIODATA DIRI</h1>
+        </div>
+
+        <div class="text-box">
+            <div class="text-tx">Nama Lengkap : Waode Makani Daga</div>
+            <div class="text-tx">Tempat/Tgl Lahir : Gowa, 29 Oktober 1993 </div>
+            <div class="text-tx">Alamat : Sapiria</div>
+            <div class="text-tx">E-mail : dagawaodemakani@gmail.com</div>
+            <div class="text-tx">Phone Number : +62813 **** ****</div>
+        </div>        
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     <img class="photo" src="{{ asset('img/A.png') }}" alt="{{ asset('img/A.png') }}">
     <div class="posisi" style="padding-left: 190px;">
         <p class="text-lg">
