b9356ce476e8cecce74104b3a7b337b0bdae2024
basriyasin <basriyasin@gmail.com>
2018-03-11 10:28:15 +0800

    Checked

--- web/Naruto/resources/views/home/beranda.blade.php
+++ web/Naruto/resources/views/home/beranda.blade.php
@@ -5,11 +5,55 @@
 @endsection
 
 @section('content')
-<!--<img class="thumbnail" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}" class="rounded-0">-->
-<!--<img class="thumbnail" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}" class="rounded-top">
-<img class="thumbnail" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}" class="rounded-right">
-<img class="thumbnail" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}" class="rounded-bottom">
-<img class="thumbnail" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}" class="rounded-left">
-<img class="thumbnail" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}" class="rounded-circle">
-<img class="thumbnail" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}" class="rounded-0">-->
+<!--<img class="thumbnail" src="{{ asset('img/project/background.jpg') }}" alt="{{ asset('img/project/background.jpg') }}">-->
+<div class="row">
+    <div class="col-md-6">
+        <div class="logo">
+            <img class="thumbnail" src="{{ asset('img/project/kampus.png') }}" alt="{{ asset('img/project/kampus.png') }}">                
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="home-page">
+            <div class="slider">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="{{ asset('img/jakarta.jpg') }}" alt="{{ asset('img/jakarta.jpg') }}">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="{{ asset('img/gorontalo.jpeg') }}" alt="{{ asset('img/gorontalo.jpeg') }}">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="{{ asset('img/luwuk.jpeg') }}" alt="{{ asset('img/luwuk.jpeg') }}">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>                
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="info-part">
+                        disini
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </div>
+</div>
+
 @endsection
