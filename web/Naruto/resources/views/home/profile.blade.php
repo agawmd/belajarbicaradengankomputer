13d022ecaf272d64fbb9da13ae65fa8648f2b0be
basriyasin <basriyasin@gmail.com>
2018-08-03 15:10:50 +0800

    Profile Upgrade

--- web/Naruto/resources/views/home/profile.blade.php
+++ web/Naruto/resources/views/home/profile.blade.php
@@ -32,7 +32,79 @@
             </div>                        
         </div>
     </div>
+    
+  
+    
+    
+    
+    <div class="col-md-8 batas-bes">
+        <div class="row">
+            <div class="col-sm-6 col-sm-1">
+                <div class="row batas-kec">
+                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/orang.png') }}" alt="{{ asset('/icon/orang.png') }}"></div>
+                    <div class="col-sm-8 col-sm-1">
+                        <div class="tulis">
+                            <div class="judul">Name :</div>
+                            <div class="isi">Waode Makani Daga</div>                    
+                        </div>                        
+                    </div>
+                </div>
+                <div class="row batas-kec">
+                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/kalender.png') }}" alt="{{ asset('/icon/kalender.png') }}"></div>
+                    <div class="col-sm-8 col-sm-1">
+                        <div class="tulis">
+                            <div class="judul">Date of Birth :</div>
+                            <div class="isi">29 October 1993</div>                    
+                        </div>                        
+                    </div>
+                </div>
+                <div class="row batas-kec">
+                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/alamat.png') }}" alt="{{ asset('/icon/alamat.png') }}"></div>
+                    <div class="col-sm-8 col-sm-1">
+                        <div class="tulis">
+                            <div class="judul">Address :</div>
+                            <div class="isi">Sapiria, <i>South Sulawesi</i></div>                    
+                        </div>                        
+                    </div>
+                </div>                
+            </div>
+            <div class="col-sm-6 col-sm-1">
+                <div class="row batas-kec">
+                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/hp.png') }}" alt="{{ asset('/icon/hp.png') }}"></div>
+                    <div class="col-sm-8 col-sm-1">
+                        <div class="tulis">
+                            <div class="judul">Phone Number :</div>
+                            <div class="isi">(+62)813-4168-6169</div>                    
+                        </div>                        
+                    </div>
+                </div>
+                <div class="row batas-kec">
+                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/email.png') }}" alt="{{ asset('/icon/email.png') }}"></div>
+                    <div class="col-sm-8 col-sm-1">
+                        <div class="tulis">
+                            <div class="judul">Send me mail :</div>
+                            <div class="isi">dagawaodemakani@gmail.com</div>                    
+                        </div>                        
+                    </div>
+                </div>
+                <div class="row batas-kec">
+                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/negara.png') }}" alt="{{ asset('/icon/negara.png') }}"></div>
+                    <div class="col-sm-8 col-sm-1">
+                        <div class="tulis">
+                            <div class="judul">Nationality :</div>
+                            <div class="isi">Indonesia</div>                    
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+
+    
+<!--    
+    
+    
     <div class="col-md-8">     
         <div class="article">
             <img class="gam" src="{{ asset('/icon/tag/orang.png') }}" alt="{{ asset('/icon/orang.png') }}">
@@ -67,7 +139,7 @@
                 <div class="isi">Indonesia</div>                    
             </div>        
         </div>        
-    </div>
+    </div>-->
 </div>
 
 @endsection
