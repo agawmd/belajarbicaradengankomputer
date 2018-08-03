c2273179efc0b50fdbfcf39c1230293af1e38847
basriyasin <basriyasin@gmail.com>
2018-08-03 19:59:42 +0800

    Resume

--- web/Naruto/resources/views/home/beranda.blade.php
+++ web/Naruto/resources/views/home/beranda.blade.php
@@ -8,12 +8,14 @@
 <!--<img class="thumbnail" src="{{ asset('img/project/background.jpg') }}" alt="{{ asset('img/project/background.jpg') }}">-->
 <div class="row">
     <div class="col-md-6">
+        <img class="foto" src="{{ asset('/img/gaya.JPG') }}">
+    </div>
+    <div class="col-md-6">
         <div class="judul">
-            <img class="foto" src="{{ asset('/img/gaya.JPG') }}">        
             <div class="halo">Hi I'm</div>
             <div class="nama">WAODE MAKANI DAGA</div>
             <div class="desk tipo">A DAUGHTER <span>|</span></div>
-            <div class="animasi"></div>
+            <div class="animasi"></div>             
         </div>
     </div>
     <!--    <div class="sesuatu"></div>-->
--- web/Naruto/resources/views/home/resume.blade.php
+++ web/Naruto/resources/views/home/resume.blade.php
@@ -9,6 +9,7 @@
     <div class="col-md-12">
         <div id="back">
             <div id="textback">Experience</div>
+            <div id="textback">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; & Education</div>
         </div>
     </div>
     <div class="col-md-12">
@@ -61,5 +62,68 @@
             </div>
         </div>
     </div>
+    
+    <div class="col-md-12">
+        <div class="judul">Education</div>
+        <div class="kepala">Formal</div>        
+    </div>
+    
+    <div class="col-md-4">
+        <div class="utama">
+            <img class="ikon" src="{{ asset('/logo/stmik.gif') }}">            
+            <div class="nama">STMIK Kharisma Makassar</div>
+            <div class="lama">2012 - Present</div>            
+        </div>
+    </div>
+    
+    <div class="col-md-8">
+        <div class="kedua">
+            <div class="prof">Software Engineering</div>
+            <div class="isi">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper dapibus felis ac consectetur. Curabitur vehicula metus et metus vestibulum, a malesuada nunc consectetur. Curabitur nunc est, vulputate id enim quis, tristique hendrerit eros. Curabitur sit amet metus ac purus lacinia varius. Donec nunc dui, ornare non fringilla ac, mollis tincidunt metus. Curabitur convallis hendrerit efficitur. Proin nisi lorem, condimentum sit amet pellentesque id, suscipit aliquet nunc. Quisque hendrerit semper odio. Suspendisse potenti. Donec pellentesque, sem vehicula aliquam facilisis, tellus ante sodales nibh, nec cursus justo augue sed nisl. Sed eu augue rutrum, mollis arcu in, posuere neque. Nullam vitae erat et diam porttitor sagittis eu sollicitudin augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla mollis ipsum diam, vitae pharetra dui rutrum eget. Quisque et diam ut ipsum gravida efficitur ut at leo. 
+                </p>
+            </div>            
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="utama">
+            <img class="ikon" src="{{ asset('/logo/twh.png') }}">            
+            <div class="nama">SMA Satria Makassar</div>
+            <div class="lama">2009 - 2012</div>            
+        </div>
+    </div>
+    
+    <div class="col-md-8">
+        <div class="kedua">
+            <div class="prof">Science Major</div>
+            <div class="isi">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper dapibus felis ac consectetur. Curabitur vehicula metus et metus vestibulum, a malesuada nunc consectetur. Curabitur nunc est, vulputate id enim quis, tristique hendrerit eros. Curabitur sit amet metus ac purus lacinia varius. Donec nunc dui, ornare non fringilla ac, mollis tincidunt metus. Curabitur convallis hendrerit efficitur. Proin nisi lorem, condimentum sit amet pellentesque id, suscipit aliquet nunc. Quisque hendrerit semper odio. Suspendisse potenti. Donec pellentesque, sem vehicula aliquam facilisis, tellus ante sodales nibh, nec cursus justo augue sed nisl. Sed eu augue rutrum, mollis arcu in, posuere neque. Nullam vitae erat et diam porttitor sagittis eu sollicitudin augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla mollis ipsum diam, vitae pharetra dui rutrum eget. Quisque et diam ut ipsum gravida efficitur ut at leo. 
+                </p>
+            </div>            
+        </div>
+    </div>
+    
+    <div class="col-md-4">
+        <div class="utama">
+            <img class="ikon" src="{{ asset('/logo/twh.png') }}">            
+            <div class="nama">SMP Satria Makassar</div>
+            <div class="lama">2006 - 2009</div>            
+        </div>
+    </div>
+    
+    <div class="col-md-8">
+        <div class="kedua">
+            <div class="prof">B Class</div>
+            <div class="isi">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper dapibus felis ac consectetur. Curabitur vehicula metus et metus vestibulum, a malesuada nunc consectetur. Curabitur nunc est, vulputate id enim quis, tristique hendrerit eros. Curabitur sit amet metus ac purus lacinia varius. Donec nunc dui, ornare non fringilla ac, mollis tincidunt metus. Curabitur convallis hendrerit efficitur. Proin nisi lorem, condimentum sit amet pellentesque id, suscipit aliquet nunc. Quisque hendrerit semper odio. Suspendisse potenti. Donec pellentesque, sem vehicula aliquam facilisis, tellus ante sodales nibh, nec cursus justo augue sed nisl. Sed eu augue rutrum, mollis arcu in, posuere neque. Nullam vitae erat et diam porttitor sagittis eu sollicitudin augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla mollis ipsum diam, vitae pharetra dui rutrum eget. Quisque et diam ut ipsum gravida efficitur ut at leo. 
+                </p>
+            </div>            
+        </div>
+    </div>    
+    
+    
 </div>
 @endsection
