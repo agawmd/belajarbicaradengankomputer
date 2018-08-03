ff54a949f9f7415d5297431c2a3459318561b525
basriyasin <basriyasin@gmail.com>
2018-08-03 15:15:35 +0800

    Profile Upgrade II

--- web/Naruto/resources/views/home/profile.blade.php
+++ web/Naruto/resources/views/home/profile.blade.php
@@ -18,25 +18,17 @@
               <div class="card-body">
                 <div class="card-text">
                     <div class="icon-bar tip">
-                        <a href="https://www.linkedin.com/in/agawmd/" target="_blank" title="Linked In"><img class="ikon" src="{{ asset('/icon/link.png') }}" alt="{{ asset('/icon/link.png') }}">
-                            
-                        </a>                        
+                        <a href="https://www.linkedin.com/in/agawmd/" target="_blank" title="Linked In"><img class="ikon" src="{{ asset('/icon/link.png') }}" alt="{{ asset('/icon/link.png') }}"></a>                        
                     </div>
                     <div class="icon-bar tip">
-                        <a href="https://www.instagram.com/aga_wmd/" target="_blank" title="Instagram"><img class="ikon" src="{{ asset('/icon/ints.png') }}" alt="{{ asset('/icon/ints.png') }}">
-                            
-                        </a>                        
+                        <a href="https://www.instagram.com/aga_wmd/" target="_blank" title="Instagram"><img class="ikon" src="{{ asset('/icon/ints.png') }}" alt="{{ asset('/icon/ints.png') }}"></a>                        
                     </div>
                 </div>
               </div>
             </div>                        
         </div>
     </div>
-    
-  
-    
-    
-    
+
     <div class="col-md-8 batas-bes">
         <div class="row">
             <div class="col-sm-6 col-sm-1">
@@ -99,47 +91,6 @@
             </div>
         </div>
     </div>
-
-
-    
-<!--    
-    
-    
-    <div class="col-md-8">     
-        <div class="article">
-            <img class="gam" src="{{ asset('/icon/tag/orang.png') }}" alt="{{ asset('/icon/orang.png') }}">
-            <div class="tulis">
-                <div class="judul">Name :</div>
-                <div class="isi">Waode Makani Daga</div>                    
-            </div>
-            <img class="gam" src="{{ asset('/icon/tag/kalender.png') }}" alt="{{ asset('/icon/kalender.png') }}">
-            <div class="tulis">
-                <div class="judul">Date of Birth :</div>
-                <div class="isi">29 October 1993</div>                    
-            </div>
-            <img class="gam" src="{{ asset('/icon/tag/alamat.png') }}" alt="{{ asset('/icon/alamat.png') }}">
-            <div class="tulis">
-                <div class="judul">Address :</div>
-                <div class="isi">Sapiria, <i>South Sulawesi</i></div>                    
-            </div>        
-
-            <img class="gam" src="{{ asset('/icon/tag/hp.png') }}" alt="{{ asset('/icon/hp.png') }}">
-            <div class="tulis">
-                <div class="judul">Phone :</div>
-                <div class="isi">(+62)813-4168-6169</div>                    
-            </div>
-            <img class="gam" src="{{ asset('/icon/tag/email.png') }}" alt="{{ asset('/icon/email.png') }}">
-            <div class="tulis">
-                <div class="judul">E-mail :</div>
-                <div class="isi">dagawaodemakani@gmail.com</div>                    
-            </div>
-            <img class="gam" src="{{ asset('/icon/tag/negara.png') }}" alt="{{ asset('/icon/negara.png') }}">
-            <div class="tulis">
-                <div class="judul">Nationality :</div>
-                <div class="isi">Indonesia</div>                    
-            </div>        
-        </div>        
-    </div>-->
 </div>
 
 @endsection
