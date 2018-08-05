6ed09e90d8557285d4b91c0f8ca5aa9f6a2618e2
basriyasin <basriyasin@gmail.com>
2018-08-05 13:46:19 +0800

    Upgrade Resume I

--- web/Naruto/resources/views/home/profile.blade.php
+++ web/Naruto/resources/views/home/profile.blade.php
@@ -57,7 +57,7 @@
         <div class="row line-middle-two">
             <div class="col-sm-12">
                 <div class="intro-box">
-                    <div class="intro">INTRODUCE</div>
+                    <div class="intro">INTRO</div>
                     <div class="intro-text">
                         Introduce me Aga, that is my nickname. I am a beginner programmer in several languages, in the last two semesters I have learned the Java language, PHP, Visual Basic (.NET) and the use of the Laravel Framework. In addition, I am interested in managing a database information system and designing user interfaces on multiple platforms.
                         The result of my first freelance work was to design the user interface and provide feedback, so that users can interact easily in using it.
@@ -69,87 +69,3 @@
 </div>
 
 @endsection
-
-<!--
-    <div class="col-md-4">
-        <div class="one">
-            <div class="card" style="width: 14rem;">
-              <img class="card-img-top" src="{{ asset('/img/3x4.JPG') }}" alt="{{ asset('/img/3x4.JPG') }}">
-              <div class="card-body">
-                <div class="card-text">
-                    <div class="pain">
-                        <div class="icon-bar tip">
-                            <a href="https://www.linkedin.com/in/agawmd/" target="_blank" title="Linked In"><img class="ikon" src="{{ asset('/icon/link.png') }}" alt="{{ asset('/icon/link.png') }}"></a>                        
-                        </div>
-                        <div class="icon-bar tip">
-                            <a href="https://www.instagram.com/aga_wmd/" target="_blank" title="Instagram"><img class="ikon" src="{{ asset('/icon/ints.png') }}" alt="{{ asset('/icon/ints.png') }}"></a>                        
-                        </div>                        
-                    </div>
-                </div>
-              </div>
-            </div>                        
-        </div>
-    </div>
-
-    <div class="col-md-8 batas-bes">
-        <div class="row">
-            <div class="col-sm-6 col-sm-1">
-                <div class="row batas-kec">
-                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/orang.png') }}" alt="{{ asset('/icon/orang.png') }}"></div>
-                    <div class="col-sm-8 col-sm-1">
-                        <div class="tulis">
-                            <div class="judul">Name :</div>
-                            <div class="isi">Waode Makani Daga</div>                    
-                        </div>                        
-                    </div>
-                </div>
-                <div class="row batas-kec">
-                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/kalender.png') }}" alt="{{ asset('/icon/kalender.png') }}"></div>
-                    <div class="col-sm-8 col-sm-1">
-                        <div class="tulis">
-                            <div class="judul">Date of Birth :</div>
-                            <div class="isi">29 October 1993</div>                    
-                        </div>                        
-                    </div>
-                </div>
-                <div class="row batas-kec">
-                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/alamat.png') }}" alt="{{ asset('/icon/alamat.png') }}"></div>
-                    <div class="col-sm-8 col-sm-1">
-                        <div class="tulis">
-                            <div class="judul">Address :</div>
-                            <div class="isi">Sapiria, <i>South Sulawesi</i></div>                    
-                        </div>                        
-                    </div>
-                </div>                
-            </div>
-            <div class="col-sm-6 col-sm-1">
-                <div class="row batas-kec">
-                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/hp.png') }}" alt="{{ asset('/icon/hp.png') }}"></div>
-                    <div class="col-sm-8 col-sm-1">
-                        <div class="tulis">
-                            <div class="judul">Phone Number :</div>
-                            <div class="isi">(+62)813-4168-6169</div>                    
-                        </div>                        
-                    </div>
-                </div>
-                <div class="row batas-kec">
-                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/email.png') }}" alt="{{ asset('/icon/email.png') }}"></div>
-                    <div class="col-sm-8 col-sm-1">
-                        <div class="tulis">
-                            <div class="judul">Send me mail :</div>
-                            <div class="isi">dagawaodemakani@gmail.com</div>                    
-                        </div>                        
-                    </div>
-                </div>
-                <div class="row batas-kec">
-                    <div class="col-sm-2 col-sm-1"><img class="gam" src="{{ asset('/icon/tag/negara.png') }}" alt="{{ asset('/icon/negara.png') }}"></div>
-                    <div class="col-sm-8 col-sm-1">
-                        <div class="tulis">
-                            <div class="judul">Nationality :</div>
-                            <div class="isi">Indonesia</div>                    
-                        </div>                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>-->
--- web/Naruto/resources/views/home/resume.blade.php
+++ web/Naruto/resources/views/home/resume.blade.php
@@ -6,7 +6,88 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-4">
+        <div class="box-tag">EXPERIENCE</div>    
+    </div>
+    <div class="col-md-8">
+        <div class="line-top"></div>
+    </div>
+       
+    <div class="col-md-4">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="work-box-1">
+                    <div class="work-icon"><img class="icon" src="{{ asset('/logo/tssc.jpg') }}" alt="{{ asset('/logo/tssc.jpg') }}"></div>
+                </div>            
+            </div>
+            <div class="col-sm-6">
+                <div class="work-box-2">
+                    <div class="work-name">Taman Surya Sport Club (Freelance)</div>
+                    <div class="work-date">Sep 2015 - Nov 2015</div>
+                    <div class="work-place">West Jakarta</div>
+                </div>            
+            </div>            
+        </div>
+    </div>
+    
+    <div class="col-md-8">
+        <div class="desc-box">
+            <div class="desc-title">UI Designer</div>
+            <div class="desc">
+                The result of my first freelance work was to design the user interface and provide feedback, so that users can interact easily in using it.
+                As I said before, I have worked as a freelance UI Designer at one of the sports clubs in West Jakarta. As a back-end programmer, it is certainly not easy, it can be said that this is the first time I know the design and implement it manually. I redesigned the workflow of the entire database that was given because at that time I was really very ordinary with the discussion, so I needed to find out what I was doing and how to solve it.
+                After that, I started with the basic technique in Photoshop CS4. Initially I would have thought this work would be short but with layers piled up to hundreds, I just realized that what I had done so far had become a large system. Which when run through PowerPoint Slideshow when it is connected via a link to the next image, is the same as when starting to run the system to end a workflow in the database.
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-md-12 line-middle"></div>
+    <div class="col-md-4">
+        <div class="box-tag">EDUCATION</div>    
+    </div>
+    <div class="col-md-8">
+        <div class="line-top"></div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="work-box-1">
+                    <div class="work-icon"><img class="icon" src="{{ asset('/logo/stmik.gif') }}" alt="{{ asset('/logo/stmik.gif') }}"></div>
+                </div>            
+            </div>
+            <div class="col-sm-6">
+                <div class="work-box-2">
+                    <div class="work-name">STMIK KHARISMA MAKASSAR</div>
+                    <div class="work-date">2012 - Present</div>
+                    <div class="work-place">South Sulawesi</div>
+                </div>            
+            </div>            
+        </div>
+    </div>
+    
+    <div class="col-md-8">
+        <div class="desc-box">
+            <div class="desc-title">Software Engineering (Bachelor's Degree)</div>
+            <div class="desc">
+                I studied Informatics study program with specialization in Software Engineering. With a long period of college, approximately 6 years are calculated this year. From the beginning I chose to enter the programming class because it was inspired by the 3 Idiots movie of that era, a reason that was crazy enough to enter and get to know a new world that I had never experienced at all.
+                When I was in school, I was a science student with the lowest Chemistry grade at the beginning of the first year of school, but with determination not to be punished and humiliated in front of other students I studied seriously until I took tutoring courses only for Chemistry. In the end, I have proven that I can and exceed other students in terms of theory and practice in Chemistry, but unfortunately I spent my college with waste of time and do nothing.
+                But I will change that mindset, which I said was not able to, then I will change it to be sure. The former never coding then I will try again and again for coding, all I start from myself.
+            </div>
+        </div>
+    </div>    
+
+    <div class="col-md-12 line-middle"></div>
+    
+    
+</div>  
+@endsection
+
+
+
+
+
+<!--    <div class="col-md-12">
         <div id="back">
             <div id="textback">Experience & Education</div>
         </div>
@@ -121,8 +202,4 @@
                 </p>
             </div>            
         </div>
-    </div>    
-    
-    
-</div>
-@endsection
+    </div>-->
