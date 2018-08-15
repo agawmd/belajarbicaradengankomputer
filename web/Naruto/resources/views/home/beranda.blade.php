30ea821cd0cf3b654013daa3cfc3918351b64c62
basriyasin <basriyasin@gmail.com>
2018-08-15 21:26:30 +0800

    Home, Profile & Resume FINISH

--- web/Naruto/resources/views/home/beranda.blade.php
+++ web/Naruto/resources/views/home/beranda.blade.php
@@ -11,30 +11,73 @@
     <div class="col-md-12">
         <div class="line-break-up"></div>
     </div>
-    
-    <div class="col-md-4">
-        <div class="image-frame">
-            <img class="image" src="{{ asset('/img/aga.png') }}" alt="{{ asset('/img/aga.png') }}">
+<!--
+    <div class="col-md-6">
+        <div class="image-box">
+            <img>here
         </div>
     </div>
+    
+    <div class="col-md-12"></div>    -->
+    
+    <div class="col-md-12">
+        <div class="text-box">
+            <div class="text-myname">W A O D E M A K A N I D A G A</div>
+        </div>
+    </div>
+    
+    <div class="col-md-12"></div>
+    
+    <div class="col-md-2"></div>
     
     <div class="col-md-8">
-        <div class="text-box">
-            <div class="first-name">WAODE</div>
-            <div class="middle-name">MAKANI DAGA</div>
-            <div class="title-name">Beginner Web Developer</div>
+        <div class="line-box">
+            <div class="underline"></div>
         </div>
     </div>
+    
+    <div class="col-md-2"></div>
+    
+    <div class="col-md-12"></div>
+    
+    <div class="col-md-12">
+        <div class="text-box">
+            <div class="text-title">I am a Beginner Programmer</div>            
+        </div>
+    </div>
+    
+    <div class="col-md-4"></div>
     
     <div class="col-md-4">
         <div class="download-box">
-            <div class="download-area">
-                <a href="{{ asset('/files/CV_Waode_Makani_Daga.pdf') }}" download="">
-                    <button class="download-button" title="Download Curriculum Vitae">DOWNLOAD MY CV <img class="icon" src="{{ asset('/icon/tag/download.png') }}" alt="{{ asset('/icon/tag/download.png') }}"></button>
-                </a>
-            </div>                   
-        </div>       
+            <a href="{{ asset('/files/CV_Waode_Makani_Daga.pdf') }}" download="">
+                <button class="download-button" title="Download Curriculum Vitae">DOWNLOAD MY CV <img class="download-icon" src="{{ asset('/icon/tag/download.png') }}" alt="{{ asset('/icon/tag/download.png') }}"></button>
+            </a>
+        </div>
     </div>
+    
+    <div class="col-md-4"></div>
+    
+    <div class="col-md-4"></div>
+    
+    <div class="col-md-4">
+        <div class="follow-box">
+            <div class="follow-text"><i>Like to read ? I am here.</i></div>
+            <div class="follow-link">
+                <div class="link-icon">
+                    <a href="#" target="_blank" title="Medium"><img class="icon" src="{{ asset('/icon/medium.png') }}" alt="{{ asset('/icon/medium.png') }}"></a>
+                </div>
+                <div class="link-icon">
+                    <a href="#" target="_blank" title="Blogger"><img class="icon" src="{{ asset('/icon/blogger.png') }}" alt="{{ asset('/icon/blogger.png') }}"></a>
+                </div>
+                <div class="link-icon">
+                    <a href="#" target="_blank" title="Wordpress"><img class="icon" src="{{ asset('/icon/wordpress.png') }}" alt="{{ asset('/icon/wordpress.png') }}"></a>
+                </div>                
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-md-4"></div>
     
     <div class="col-md-12">
         <div class="line-break-down">
@@ -44,3 +87,22 @@
 </div>
 
 @endsection
+
+<!--    
+    <div class="col-md-4">
+        <div class="image-frame">
+            <img class="image" src="{{ asset('/img/aga.png') }}" alt="{{ asset('/img/aga.png') }}">
+        </div>
+    </div>
+    -->
+    
+<!--    <div class="col-md-4">
+        <div class="download-box">
+            <div class="download-area">
+                <a href="{{ asset('/files/CV_Waode_Makani_Daga.pdf') }}" download="">
+                    <button class="download-button" title="Download Curriculum Vitae">DOWNLOAD MY CV <img class="icon" src="{{ asset('/icon/tag/download.png') }}" alt="{{ asset('/icon/tag/download.png') }}"></button>
+                </a>
+            </div>                   
+        </div>       
+    </div>
+    -->    
